2d66d7a840bee26c3ac9c811cf2fdb6a462b1167
itggot-august-borg <august.borg@itggot.se>
2018-04-26 10:43:50 +0200

    errorfix
    inge

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ class App < Sinatra::Base
 
 	require 'sinatra'
 	require 'sqlite3'
+	require 'erb'
 	
 	get('/') do
 		erb(:index)
@@ -12,10 +13,12 @@ class App < Sinatra::Base
 	end
 	
 	post('/login') do
-		user_name = params['user-name']
+		user_name = params['username']
+		password  = params['password']
 	
 		db = SQLite3::Database.new('db/login.sqlite')
 		db.execute("INSERT INTO users('Name') VALUES(?)", [user_name])
+		db_password = db.execute('select password FROM Users where username=?',[user_name])[0][0]
 		result = db.execute("SELECT UserId FROM users WHERE Name=?", [user_name])
 		user_id = result[0][0]
 		
